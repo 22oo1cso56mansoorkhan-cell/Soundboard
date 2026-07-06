0201b958275ce340a4faf0ca763207b57da61fcb
Mansoor Khan <22OO1CSO56mansoorkhan@gmail.com>
2026-07-06 15:39:07 +0530

    Initial HTML structure + CSS reset + JS sound list

---
+++ script.js
@@ -0,0 +1,14 @@
+const sounds = [
+  { name: 'Applause', icon: 'fa-regular fa-circle-check' },
+  { name: 'Bass', icon: 'fa-solid fa-bass' },
+  { name: 'Bell', icon: 'fa-solid fa-bell' },
+  { name: 'Clap', icon: 'fa-regular fa-hand' },
+  { name: 'Crash', icon: 'fa-solid fa-bolt' },
+  { name: 'Drum', icon: 'fa-solid fa-drum' },
+  { name: 'Guitar', icon: 'fa-solid fa-guitar' },
+  { name: 'Hi-hat', icon: 'fa-solid fa-hat-cowboy' },
+  { name: 'Kick', icon: 'fa-solid fa-shoe-prints' },
+  { name: 'Snare', icon: 'fa-solid fa-drumstick' },
+  { name: 'Synth', icon: 'fa-solid fa-wave-square' },
+  { name: 'Wood', icon: 'fa-solid fa-tree' }
+];
